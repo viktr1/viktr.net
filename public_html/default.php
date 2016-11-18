204370e3d86997373dc9032fba1906dbf905f9e9
Viktor <redwiggeztv@gmail.com>
2016-11-18 15:23:26 +0100

    Create default.php

---
+++ public_html/default.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en_US">
+	<head>
+		<link rel="stylesheet" href="style.css">
+		<title>Your average website</title>
+	</head>
+	<body>
+
+			<ul>    |
+				<li><a href ="https://github.com/viktr1" target="_blank">github</a></li> 
+                                |
+				<li><a href ="https://discord.gg/sX4bNMp" target="_blank">discord</a></li>
+                                |
+				<li><a href ="mailto:contact@viktr.net" target="_blank">contact</a></li> 
+                                |
+				<li><a href ="http://steamcommunity.com/profiles/76561198059301340/" target="_blank">steam</a></li> 
+                                |
+                                <li><a href ="http://twitch.tv/redwiggez" target="_blank">twitch</a></li> 
+                                |
+                                <li><a href ="https://soundcloud.com/redwiggez" target="_blank">music</a></li> 
+			        |
+</ul>   
+<center>
+<script type="text/javascript">
+  var imageURLs = [
+       "http://i.imgur.com/5hdj3Iq.png"
+     , "http://i.imgur.com/NA7IzPc.png"
+     , "https://cdn.discordapp.com/attachments/163308115952009216/249173006671151114/sunker.png"
+  ];
+  function getImageTag() {
+    var img = '<img src=\"';
+    var randomIndex = Math.floor(Math.random() * imageURLs.length);
+    img += imageURLs[randomIndex];
+    img += '\" alt=\"Some alt text\"/>';
+    return img;
+  }
+</script>
+</head>
+<body>
+<script type="text/javascript">
+  document.write(getImageTag());
+</script>
+
+</center>
+
+<p hidden>//credit for js: http://stackoverflow.com/questions/2777479/display-random-image-when-page-loads-without-utilizing-onload-in-the-body-tag</p>
+
+	</body>
+</html>	
